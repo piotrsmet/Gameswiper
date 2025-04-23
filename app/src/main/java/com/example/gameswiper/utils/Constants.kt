c24ddba059f236ed0ad7724098f3f6631f4f03e4
smecik48 <53114567+smecik48@users.noreply.github.com>
2025-04-23 14:51:08 +0200

    Update Constants.kt

--- app/src/main/java/com/example/gameswiper/utils/Constants.kt
+++ app/src/main/java/com/example/gameswiper/utils/Constants.kt
@@ -3,11 +3,13 @@ package com.example.gameswiper.utils
 import com.example.gameswiper.model.Genre
 import com.example.gameswiper.model.Platform
 
+//you need to get these values on your own
+val CLIENT_ID = ""
+val CLIENT_SECRET =  ""
+var ACCESS_TOKEN: String? = ""
+var EXPIRES_IN: Long = 0
 
-val CLIENT_ID = "p974jcjhdift50t7313a0qaopxe37x"
-val CLIENT_SECRET =  "eezehbbjj53uvwm0u9ead5r5qq0d62"
-var ACCESS_TOKEN: String? = "fil0uib3vgt2j6q29jax75mflcva17"
-var EXPIRES_IN: Long = 4736569
+
 val PLATFORMS: List<Platform> = listOf(
     Platform(6, "PC"),
     Platform(167, "PlayStation 5"),
